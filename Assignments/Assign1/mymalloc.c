b87c21cca79c5a266d8d65b236a537a76bc989ba
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2017-02-23 20:14:48 -0500

    Finished mymalloc and myfree

--- Assignments/Assign1/mymalloc.c
+++ Assignments/Assign1/mymalloc.c
@@ -3,8 +3,6 @@
 #include <unistd.h>
 #include <string.h>
 
-#define MAX_MEM 5000
-
 typedef struct memoryAllocation{
   size_t numBytes;
   int free;
@@ -13,8 +11,11 @@ typedef struct memoryAllocation{
 } node;
 
 static node *head;
-node* prev = NULL;
-node* current = head;
+
+
+//prototypes
+void* changeMemory(node *mem,size_t numBytes,int free,void *ptr,node *next);
+int clean(node* mem, int place);
 
 void* mymalloc(size_t numBytes, const char *file, const int lineNum){
   //no entries
@@ -26,13 +27,14 @@ void* mymalloc(size_t numBytes, const char *file, const int lineNum){
     changeMemory(head, numBytes, 0, head+1, NULL);
     return head->ptr;
   }
-
+  node* prev = NULL;
+  node* current = head;
   while(current){
     size_t difference = current->numBytes - numBytes;
     if(current->free){
       if(difference==0){
         current->free = 0;
-      }else if(diff>0){
+      }else if(difference>0){
         current->free=0;
       }
       return current->ptr;
@@ -53,12 +55,77 @@ void* mymalloc(size_t numBytes, const char *file, const int lineNum){
 }
 
 
+void myFree(void *ptr, const char *file, const int lineNum){
+  node *prev = NULL;
+  node *current = head;
 
-//-------------------------------------------------------------------------------
-changeMemory(node *mem, size_t numBytes, int free, void *ptr, node *next){
+  while(current){
+    if(current->ptr == ptr){
+      if(current->free){
+        printf("Pointer already freed on line: %d, in file %s\n", lineNum, *file);
+        return;
+      }
+      current->free = 1;
+      //combining 2 free blocks if possible
+      if(current->next && current->next->free){
+        changeMemory(current, current->numBytes + current->next->numBytes + sizeof(node), 1, current->ptr, current->next->next);
+      }
+      if(prev && prev->free){
+        changeMemory(prev, prev->numBytes+current->numBytes + sizeof(node),1, prev->ptr, current->next);
+      }
+      clean(head, 0);
+      return;
+    }
+    prev=current;
+    current=current->next;
+
+  }
+  printf("ERROR Pointer not given on line: %d, in file: %s\n", lineNum, *file);
+  return;
+}
+
+
+
+//--------------------------------Extra Help Methods-------------------------------------
+void changeMemory(node *mem, size_t numBytes, int free, void *ptr, node *next){
     mem->numBytes = numBytes;
     mem->free = free;
     mem->ptr = ptr;
     mem->next = next;
     return;
+}
+
+
+/* Return integer meanings
+ * 0=Used
+ * 1=Free
+ * 2=NULL
+ */
+int clean(node* mem, int place){
+  if(!mem){
+    return 2;
+  }
+
+  int iteration = clean(mem->next, place+1);
+  if(iteration==0){
+    return 0;
+  }else if(iteration==1){
+    sbrk((mem->next->numBytes + sizeof(node))*-1);
+    mem->next=NULL;
+
+    if(mem->free && place==0){
+      sbrk((mem->numBytes+sizeof(node))*-1);
+      head=NULL;
+      return 0;
+    }
+
+    return mem->free;
+  }
+  if(mem->free && place==0){
+    sbrk((mem->numBytes+sizeof(node))*-1);
+    head=NULL;
+    return 0;
+  }
+  return mem->free;
+
 }

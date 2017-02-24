0f4b907a878c06b2876fe492213218c6849b0537
Joseph Moussa <jmoussa@Josephs-MacBook-Pro.local>
2017-02-23 20:21:13 -0500

    Refined mymalloc.h

--- Assignments/Assign1/mymalloc.h
+++ Assignments/Assign1/mymalloc.h
@@ -1,11 +1,11 @@
-#ifndef mymalloch
-#define mymalloch
+#ifndef mymalloc
+#define mymalloc
 
 #include <stdlib.h>
 
 void* mymalloc(size_t numBytes, const char *filename, const int lineNumber);
 
-void* myfree(void *ptr, const char *filename, const int lineNumber);
+void myfree(void *ptr, const char *filename, const int lineNumber);
 
 #define malloc(x) mymalloc((x),_FILE_,_LINE_)
 #define free(x) myfree((x),_FILE_,_LINE_)
